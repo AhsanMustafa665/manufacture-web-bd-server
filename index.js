bc5f4549f685d2cb341f71a6ce63cb68931b6259
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-05-24 21:34:07 +0600

    tools services added

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v1xk5w7.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("tools-portal").collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Manufacturer-website is running");
 });
